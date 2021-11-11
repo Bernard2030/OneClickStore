b5de9072a021a7a2d73e6bd755453ecfa92cd316
Kennedy-Mwaura <kemwaura@gmail.com>
2021-11-12 00:58:25 +0300

    settings for GitHub workflow file

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -80,6 +80,18 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 
 MODE = os.environ.get('MODE', default="dev")
 DEBUG = os.environ.get('DEBUG', default=True)
+# GitHub Workflow settings
+if os.getenv('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github-actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'localhost',
+            'PORT': '5432'
+        }
+    }
 # development
 if MODE == "dev":
     DATABASES = {
