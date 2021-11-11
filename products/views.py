4e339136e80488730ae876185fb8d4e940cd0e3b
Kennedy-Mwaura <kemwaura@gmail.com>
2021-11-12 01:57:58 +0300

    added views for the products, homepage

--- products/views.py
+++ products/views.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
+from django.views.generic import DeleteView, ListView
+from rest_framework import viewsets
+from .serializers import ProductSerializer
+from .models import Product
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'products/index.html')
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('-date_added')
+    serializer_class = ProductSerializer
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
+    ordering = ['-date_added']
+    paginate_by = 10
+
+
+class ProductDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Product
+    success_url = '/'
+
+    def test_func(self):
+        product = self.get_object()
+        if self.request.user == product.owner:
+            return True
+        return False
