3c4417e7a74691bfecd9a2fa9baae9c204c4f250
Bernard Opiyo <brobernard.254@gmail.com>
2021-11-11 18:49:19 +0300

    add change password serializer class in authentication

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -56,4 +56,36 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.set_password(validated_data.get('password'))
         user.save()
         return user  
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    """
+    Serializer for password change
+    """
+    password = serializers.CharField(write_only=True,required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True,required=True)
+    old_password = serializers.CharField(write_only=True,required=True)
+
+    class Meta:
+        model = User
+        fields = ('old_password','password', 'password2', )
+
+
+    def validate(self, attrs):
+        if attrs('password') != attrs('password2'):
+                raise serializers.ValidationError({"password": "Passwords must match"})
+                
+        return attrs 
+
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError({"old_password":"Old password is incorrect"})
+
+        return value 
+
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data('password'))
+        instance.save()
+        return instance              
  
