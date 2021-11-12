fc51aff4baa1164c74122e137f8c801ee387bcd5
Kennedy-Mwaura <kemwaura@gmail.com>
2021-11-12 21:44:29 +0300

    changed image to upload to Cloudinary

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
@@ -6,7 +7,8 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(null=True, blank=True)
+    # image = models.ImageField(null=True, blank=True)
+    image = CloudinaryField('image', null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
