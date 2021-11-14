d8c5555b8bf3137be0fcdf0ba62cbda57d8d39a7
Kennedy-Mwaura <kemwaura@gmail.com>
2021-11-15 00:54:07 +0300

    Added user models amd ratings to admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, UserProfile, UserRating
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(UserProfile)
+admin.site.register(UserRating)
+
