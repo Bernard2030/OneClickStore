ae4b2205629d2e3a9211b549d641d6691415b8c3
Kennedy-Mwaura <kemwaura@gmail.com>
2021-11-15 01:09:19 +0300

    Created tests for the user profile model

--- products/tests.py
+++ products/tests.py
@@ -43,3 +43,75 @@ class ProductTests(TestCase):
         test_price = Product(price=10.00)
         self.assertEqual(len(str(test_price.price)), 4)
 
+    def test_product_price_length_long(self):
+        test_price = Product(price=100000.00)
+        self.assertEqual(len(str(test_price.price)), 8)
+
+
+class UserProfileTests(TestCase):
+    """
+    Here we'll define the tests that we'll run against our UserProfile models
+    """
+
+    def setUp(self):
+        self.user = User(id=1, username='zoo', password='testpwsd123')
+        self.user.save()
+        self.user_2 = User(id=2, username='zoo2', password='testpwsd123')
+        self.user_2.save()
+
+    """
+        @classmethod
+        def setUpTestData(cls):
+            UserProfile.objects.create(user=User.objects.get(id=1))
+            UserProfile.objects.create(user=User.objects.get(id=2))
+    """
+
+    def tearDown(self):
+        self.user = None
+        self.user_2 = None
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
+
+    def test_update_user(self):
+        self.user.username = 'zken'
+        self.user.save()
+
+    def test_get_user(self):
+        self.user.username = 'zken'
+        self.user.save()
+        user = User.objects.get(id=1)
+        self.assertEquals(user.username, 'zken')
+
+    def test_get_all_users(self):
+        self.user.username = 'zken'
+        self.user_2.username = 'zken2'
+        self.user.save()
+        self.user_2.save()
+        users = User.objects.all()
+        self.assertEquals(len(users), 2)
+
+    def test_get_all_users_by_id(self):
+        self.user.username = 'zken'
+        self.user.save()
+        users = User.objects.all()
+        self.assertEquals(users[0].id, 2)
+
+    def test_get_all_users_by_username(self):
+        self.user.username = 'zken'
+        self.user.save()
+        users = User.objects.all()
+        self.assertEquals(users[1].username, 'zken')
+
+    def test_get_user_profile(self):
+        self.user.username = 'zken'
+        self.user.save()
+        user_profile = UserProfile.objects.get(user=self.user)
+        self.assertEquals(user_profile.user, self.user)
+
