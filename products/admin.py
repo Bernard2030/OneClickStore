20e05486c87eb0d9b2be1a917d1361f7807872e3
Kennedy-Mwaura <kemwaura@gmail.com>
2021-11-15 15:37:13 +0300

    Added product sale and category to admin

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Product, UserProfile, UserRating
+from .models import Product, UserProfile, UserRating, ProductSale, Category
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(UserProfile)
 admin.site.register(UserRating)
+admin.site.register(ProductSale)
+admin.site.register(Category)
 
