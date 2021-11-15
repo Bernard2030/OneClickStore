c2fe72cbd721691113cf5473cd07eb87becf7d61
Kennedy-Mwaura <kemwaura@gmail.com>
2021-11-15 15:37:32 +0300

    Added product sale model

--- products/models.py
+++ products/models.py
@@ -160,3 +160,35 @@ class UserReview(models.Model):
     def get_date(self):
         return self.date
 
+
+class ProductSale(models.Model):
+    """
+    Product Sale model
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_sales", null=True)
+    buyer = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="buyer_sales", null=True)
+    seller = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="seller_sales", null=True)
+    date_sold = models.DateTimeField(auto_now_add=True, blank=True)
+    quantity = models.IntegerField(default=0, blank=True)
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2, default=0, blank=True)
+
+    def __str__(self):
+        return f'{self.product.name}: sale'
+
+    def get_absolute_url(self):
+        return "/products/{}".format(self.id)
+
+    def get_product(self):
+        return self.product
+
+    def get_product_id(self):
+        return self.product.id
+
+    def get_date(self):
+        return self.date_sold
+
+    def get_quantity(self):
+        return self.quantity
+
+    def get_total_price(self):
+        return self.sale_price
