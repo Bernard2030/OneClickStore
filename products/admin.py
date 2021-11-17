0aa83631bcdf44118cd9cc67388b86b936e87ba3
Kennedy-Mwaura <kemwaura@gmail.com>
2021-11-17 22:30:03 +0300

    workflow fixes for CI build

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Product, UserProfile, UserRating, ProductSale, Category
 
+
+admin.site.site_header = "OneClick Admin"
+
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(UserProfile)
@@ -8,5 +11,4 @@ admin.site.register(UserRating)
 admin.site.register(ProductSale)
 admin.site.register(Category)
 
-admin.site.site_header = "OneClick Admin"
 
