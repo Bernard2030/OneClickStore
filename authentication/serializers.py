4d42323b7fde1e3817fe70bddeaec39e6f211949
Bernard Opiyo <brobernard.254@gmail.com>
2021-11-18 21:05:07 +0300

    remove password 2 requirement

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -31,11 +31,11 @@ class RegisterSerializer(serializers.ModelSerializer):
     )
 
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
+    
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'password', 'password2', 'email', 'first_name', 'last_name')
+        fields = ('id', 'username', 'password', 'email', 'first_name', 'last_name')
         extra_kwargs = {'first_name': {'required': True}, 'last_name': {'required': True}}
 
     def validate(self, attrs):
